c6663b69a97e62484d58d638a9dee3010572ee86
Wai Ling Ye <wai.ling.ye@gmail.com>
2018-11-19 16:27:11 -0500

    Fix autofill.js

--- auto_fill.js
+++ auto_fill.js
@@ -1,16 +1,17 @@
 function autofill(){
-  var total = document.querySelectorAll('.recipe').length
+  var total = document.getElementsByClassName('recipe').length;
 
   for (i=0; i <= total; i++) {
     //generate random number between 1 and 4
-    var min=1;
+    var min=0;
     var max=4;
     var random = Math.floor(Math.random() * (+max - +min)) + +min;
 
     //Fill recipe class with random recipe
-    var num = i + 1;
+    var recipes = document.getElementsByClassName('recipe');
+    var presets = document.getElementsByClassName('recipe_preset');
 
-    document.getElementByClassName('recipe:nth-of-type(' + num + ')').innerHTML = document.getElementByClassName('recipe' + random + ')').innerHTML;
+    recipes[i].innerHTML = presets[random].innerHTML;
   }
 }
 
