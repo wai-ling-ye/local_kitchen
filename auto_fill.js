5482453791d7fecf5f10dd7422008b85c4de2e04
Wai Ling Ye <wai.ling.ye@gmail.com>
2018-11-19 16:55:39 -0500

    Update file names

--- auto_fill.js
+++ auto_fill.js
@@ -2,7 +2,7 @@ function autofill(){
   var total = document.getElementsByClassName('recipe').length;
 
   for (i=0; i <= total; i++) {
-    //generate random number between 1 and 4
+    //generate random number between 0 and 3
     var min=0;
     var max=4;
     var random = Math.floor(Math.random() * (+max - +min)) + +min;
