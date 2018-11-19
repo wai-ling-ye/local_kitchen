70efc643fc62606a4d6836bf7977e496ee3b2053
Wai Ling Ye <wai.ling.ye@gmail.com>
2018-11-18 19:11:27 -0500

    Update HTML & CSS; Add auto-fill.js and image files

---
+++ auto_fill.js
@@ -0,0 +1,17 @@
+function autofill(){
+  var total = document.querySelectorAll('.recipe').length
+
+  for (i=0; i <= total; i++) {
+    //generate random number between 1 and 4
+    var min=1;
+    var max=4;
+    var random = Math.floor(Math.random() * (+max - +min)) + +min;
+
+    //Fill recipe with random recipe
+    var num = i + 1;
+
+    document.getElementByClassName('recipe:nth-of-type(' + num + ')').innerHTML = document.getElementByClassName('recipe' + random + ')').innerHTML;
+  }
+}
+
+window.onload = autofill;
